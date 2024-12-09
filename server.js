6e45577cc36ac7db0026dd0f1990f53e99998c2c
Antonio Tito <titojunnio1@gmail.com>
2024-12-08 22:16:50 -0300

    levando pro git

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Servir arquivos estáticos da pasta 'public' (HTML principal e CSS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Servir arquivos estáticos da pasta 'modelagens' (PDFs e HTMLs de processos)
+app.use('/modelagens', express.static(path.join(__dirname, 'modelagens')));
+
+// Rota principal que serve o arquivo portfolio.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'portifolio.html'));
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
